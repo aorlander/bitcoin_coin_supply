073a0c68581701913489cce3da4551764eabe9f0
aorlander <49505752+aorlander@users.noreply.github.com>
2021-02-24 11:56:35 -0500

    x

--- num_BTS.py
+++ num_BTS.py
@@ -6,21 +6,19 @@ from pathlib import Path
 # Input is block height (integer)
 # Return total number of tokens that have been mined so far (up to and including the given block)
 def num_BTC(b):
-    max = 210000
     r = 50
     n = 0
     count = 0
 
     while count < b :
+        count = count + 1
         n = n + r
         i = int(n)
-        if pow(i, 1, max) == 0:   # Reward halves every 210k blocks
+        if pow(i, 1, 210000) == 0:   # Reward halves every 210k blocks
             r = r/2 
-        count = count + 1
-    
-    c = float(n)
-    print(c)
-    return c
+            
+    print(float(n))
+    return float(n)
 
 # test cases
 num_BTC(1)
