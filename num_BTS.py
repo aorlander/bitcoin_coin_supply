13a69d4fe7df193c99d93a1ed366e26195ed2f88
aorlander <49505752+aorlander@users.noreply.github.com>
2021-02-24 11:17:19 -0500

    Clean up

--- num_BTS.py
+++ num_BTS.py
@@ -1,10 +1,6 @@
 import math
 
 # Return total number of tokens that have been mined so far (up to and including the given block)
-# Bitcoin tokens are generated through block rewards. 
-# Initially, the block reward was 50 BTC, and the reward halves every 210,000 blocks. 
-# Thus at Block 1 there were 50 BTC in circulation. At Block 2, there were 100 BTC etc.
-
 def num_BTC(b):
     max = 210000
     r = 50
@@ -14,8 +10,7 @@ def num_BTC(b):
     while count < b:
         n = n + r
         i = int(n)
-        split = pow(i, 1, max)
-        if split == 0: # reward halves every 210k blocks
+        if pow(i, 1, max) == 0:   # Reward halves every 210k blocks
             r = r/2 
         count = count + 1
     
@@ -23,5 +18,3 @@ def num_BTC(b):
     return c
 
 
-blockHeight = 14055
-num_BTC(blockHeight)
