1873318e2aa635fea6d4b249872f8e1bb30355cb
aorlander <49505752+aorlander@users.noreply.github.com>
2021-02-24 21:50:49 -0500

    float

--- num_BTC.py
+++ num_BTC.py
@@ -6,8 +6,8 @@ from pathlib import Path
 # Input is block height (integer)
 # Return total number of tokens that have been mined so far (up to and including the given block)
 def num_BTC(b):
-    reward = 50
-    tok = 0
+    reward = 50.00
+    tok = 0.00
     height = 1
     max = 210000
 
@@ -18,8 +18,7 @@ def num_BTC(b):
         remainder = pow(tok_int, 1, max)
         if remainder == 0:   # Reward halves every 210k blocks
             reward = reward/2 
-            
-    print(float(tok))
+  
     return float(tok)
 
 # test cases
